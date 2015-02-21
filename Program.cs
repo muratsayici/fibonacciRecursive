b03f9e9c760e5c9478d331eb976812e683ebcae0
muratsayici <muratsayici@gmail.com>
2015-02-21 23:30:21 +0200

    fibonacci sayıları
    fibonacci sayılarını özyinelemeli olarak bulur

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public static Stopwatch watch = new Stopwatch();
+
+        static void Main(string[] args)
+        {
+            long deger = 0;
+            Console.Write("Kacinci Fibonacciyi Bulayim : ");
+            deger = Convert.ToInt64(Console.ReadLine());
+    
+            watch.Start();
+            Console.WriteLine("{0} . Fibonacci: {1}", deger, recursive(deger));
+            watch.Stop();
+
+            Console.WriteLine("Gecen sure: {0} ", watch.Elapsed.ToString());
+            Console.ReadLine();
+
+        }
+        public static long recursive(long girdi)
+        {
+
+            if (girdi == 1 || girdi == 2)
+            {
+                return 1;
+            }
+            else
+
+                return (recursive(girdi - 1) + recursive(girdi - 2));
+        }
+    }
+}
